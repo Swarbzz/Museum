fd753f9f8f31b16130b0b0a7e1b2280656306951
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-02-26 13:27:44 +0000

    Added a failing test

--- Lib/artist.rb
+++ Lib/artist.rb
@@ -6,3 +6,8 @@ class Artist
     @first_name = first_name
     @last_name = last_name
   end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+end
--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -1,3 +1,8 @@
 require "artist"
 
-describe
+describe Artist do
+  it "says the artist first and last names" do
+    subject(first_name, last_name)
+    expect(subject.full_name).to eq "#{first_name}, #{last_name}"
+  end
+end
